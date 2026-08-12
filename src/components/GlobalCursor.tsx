daea747295559073c0dbe4aee5203065c2c5c28b
Ajg-horizen <ajg@horizen.dk>
2026-08-12 11:15:13 +0200

    Oprydning: fjern 11 ubrugte/doede komponent-filer

--- src/components/GlobalCursor.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const GlassCursor = dynamic(() => import("@/components/GlassCursor"), { ssr: false });
-
-export default function GlobalCursor() {
-  return <GlassCursor />;
-}
